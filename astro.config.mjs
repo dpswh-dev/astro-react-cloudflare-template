026d6f79852ff81897090a3c59237634d7cf5709
dpswh-dev <lanutanenzo.developer@gmail.com>
2025-01-01 19:53:08 +0800

    chore: add react, mdx, tailwind

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
+
+  integrations: [react(), mdx(), tailwind()]
+});
